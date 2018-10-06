9ad51ce04544883de803dcbfd727333bb1f7fbbb
Folamomo <noga.igor@gmail.com>
2018-10-06 12:37:04 +0200

    Pierwsze

--- src/sbobek/lab1/LiczbyPierwsze.java
+++ src/sbobek/lab1/LiczbyPierwsze.java
@@ -1,7 +1,17 @@
 package sbobek.lab1;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class LiczbyPierwsze {
-    public static void main(){
-        
+    public static void main(String[] args){
+        System.out.println("Podaj liczbę maksymalną");
+        Scanner in = new Scanner(System.in);
+        int max = in.nextInt();
+        ArrayList primes = durszlak.getPrimes(max);
+        for (Object i:primes) {
+            System.out.println(i);
+        }
     }
 }
