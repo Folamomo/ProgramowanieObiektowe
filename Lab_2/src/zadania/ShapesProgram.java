5d550727f9a3c1e35eaea282dd6e943ca47683f1
Folamomo <noga.igor@gmail.com>
2018-11-25 21:51:11 +0100

    Lab4

---
+++ Lab_2/src/zadania/ShapesProgram.java
@@ -0,0 +1,11 @@
+package zadania;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class ShapesProgram {
+    private ArrayList shapes = new ArrayList<Shape>();
+    public static void main(String[] args) {
+        
+    }
+}
