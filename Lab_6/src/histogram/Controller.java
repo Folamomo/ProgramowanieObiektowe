61746da8a6b20ceafa431c59604299de4aa1db0b
Folamomo <noga.igor@gmail.com>
2018-12-19 02:48:08 +0100

    Histogram

--- Lab_6/src/histogram/Controller.java
+++ Lab_6/src/histogram/Controller.java
@@ -26,8 +26,13 @@ public class Controller implements Initializable {
     public XYChart.Series series= new BarChart.Series();
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        for (int i = 0; i < 20; i++) {
+            histogram.add(Integer.toString(random.nextInt()), random.nextGaussian());
+        }
         Chart.getData().addAll(series);
-
+        for (Map.Entry<Double, Integer> entry : histogram.getProcessedData().entrySet()){
+            series.getData().add(new XYChart.Data(Double.toString(entry.getKey()), entry.getValue()));
+        }
     }
 
 
@@ -36,7 +41,7 @@ public class Controller implements Initializable {
         histogram.add(Integer.toString(random.nextInt()), random.nextGaussian());
         series.getData().clear();
         for (Map.Entry<Double, Integer> entry : histogram.getProcessedData().entrySet()){
-            series.getData().add(new BarChart.Data(Double.toString(entry.getKey()), entry.getValue()));
+            series.getData().add(new XYChart.Data(Double.toString(entry.getKey()), entry.getValue()));
         }
 
     }
