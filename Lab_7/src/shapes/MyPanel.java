443b5c3f231cb0fed93e7b8b7c5a374e8ccdff68
Folamomo <noga.igor@gmail.com>
2018-12-20 05:07:16 +0100

    Shapes

---
+++ Lab_7/src/shapes/MyPanel.java
@@ -0,0 +1,22 @@
+package shapes;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.LinkedList;
+
+public class MyPanel extends JPanel {
+    public LinkedList<Shape> shapesList = new LinkedList<>();
+
+    public MyPanel() {
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.WHITE);
+        g.fillRect(0,0,this.getWidth(),this.getHeight());
+        for(Shape s: shapesList) {
+            s.draw(g);
+        }
+    }
+
+
+}
